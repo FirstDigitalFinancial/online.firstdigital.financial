d057a2ab7b431bbba647540042a557d92158d289
Andy McCall <mailme@andymccall.co.uk>
2017-09-10 22:01:48 +0100

    Added UserType

---
+++ src/main/java/financial/firstdigital/online/model/UserType.java
@@ -0,0 +1,19 @@
+package financial.firstdigital.online.model;
+
+/**
+ * The UserType enumeration details the types of users the system
+ * comtains
+ *
+ * @author  Andy McCall
+ * @version 0.1
+ * @since   2017-09-10
+ */
+
+public enum UserType {
+    ADMINISTRATOR,
+    CUSTOMER_SUPPORT,
+    CUSTOMER_SALES,
+    BROKER_SUPPORT,
+    BROKER_SALES,
+    CUSTOMER
+}
