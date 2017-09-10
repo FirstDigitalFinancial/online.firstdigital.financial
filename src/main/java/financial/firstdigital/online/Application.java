b7edaea70b7f76bc83cfe0e3624b727c06b764e6
Andy McCall <mailme@andymccall.co.uk>
2017-09-10 20:00:09 +0100

    Initial commit

---
+++ src/main/java/financial/firstdigital/online/Application.java
@@ -0,0 +1,28 @@
+package financial.firstdigital.online;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The Application class is the entry point to the main program
+ *
+ * @author  Andy McCall, andy.mccall@firstdigtial.financial
+ * @version 0.1
+ * @since   2017-09-03
+ */
+@SpringBootApplication
+public class Application {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting SpringApplication.run()");
+
+        SpringApplication.run(Application.class, args);
+
+    }
+
+}
---
+++ src/test/java/financial/firstdigital/online/ApplicationTest.java
@@ -0,0 +1,16 @@
+package financial.firstdigital.online;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.validation.constraints.AssertTrue;
+
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+    @Test
+    public void main() throws Exception {
+        Assert.assertTrue(true);
+    }
+
+}
