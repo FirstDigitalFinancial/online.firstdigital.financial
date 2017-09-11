cb057f2ab40908116f921faeebe3a7fd3250dc51
Andy McCall <mailme@andymccall.co.uk>
2017-09-11 22:27:03 +0100

    Added Spring security configuration class

---
+++ src/main/java/financial/firstdigital/online/configuration/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package financial.firstdigital.online.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * The SecurityConfiguration class is the class that set
+ * Spring Security configuration
+ *
+ * @author  Andy McCall
+ * @version 0.2
+ * @since   2017-09-11
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/user/**", "/includes/**").permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+//        auth.userDetailsService(userDetailsService)
+//                .passwordEncoder(passwordEncoder());
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+}
