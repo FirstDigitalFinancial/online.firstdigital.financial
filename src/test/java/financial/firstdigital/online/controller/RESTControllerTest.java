bf2550ddeba82f5361d470dfb7643251634633a3
Andy McCall <mailme@andymccall.co.uk>
2017-10-17 21:19:48 +0100

    Added RESTControllerTest and in memory database for testing

---
+++ src/test/java/financial/firstdigital/online/controller/RESTControllerTest.java
@@ -0,0 +1,93 @@
+package financial.firstdigital.online.controller;
+
+import financial.firstdigital.online.Application;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * The RESTControllerTest class is a test class
+ * that tests RESTController.
+ *
+ * @author  Andy McCall
+ * @version 0.1
+ * @since   2017-10-17
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+@TestPropertySource(
+        locations = "classpath:test.properties")
+public class RESTControllerTest {
+
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+    private MockMvc mockMvc;
+
+    private HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+    @InjectMocks
+    private RESTController restController;
+
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+
+        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
+                .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
+                .findAny()
+                .orElse(null);
+
+        assertNotNull("the JSON message converter must not be null",
+                this.mappingJackson2HttpMessageConverter);
+    }
+
+    /**
+     * Sets up objects and mocks external dependencies needed
+     * for the tests.
+     */
+    @Before
+    public void setUp() throws Exception {
+        // this must be called for the @Mock annotations above to be processed
+        // and for the mock service to be injected into the controller under
+        // test.
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(restController).build();
+
+    }
+
+    @Test
+    public void getPing_PingIsGot_Passes() throws Exception {
+        mockMvc.perform(get("/ping"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("$.result[0].response", is("pong")));
+    }
+
+}
