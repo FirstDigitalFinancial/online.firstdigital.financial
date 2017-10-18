d40816c88f7c96a7a407c1d043f85045d0757871
Andy McCall <mailme@andymccall.co.uk>
2017-10-18 21:29:36 +0100

    Added tests for EmailDetailServiceImpl

---
+++ src/test/java/financial/firstdigital/online/service/EmailDetailServiceImplTest.java
@@ -0,0 +1,91 @@
+package financial.firstdigital.online.service;
+
+import financial.firstdigital.online.model.EmailDetail;
+import financial.firstdigital.online.model.EmailType;
+import financial.firstdigital.online.repository.EmailDetailRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+/**
+ * The EmailDetailServiceImplTest class is a test class that
+ * tests the EmailDetailServiceImpl class
+ *
+ * @author  Andy McCall
+ * @version 0.1
+ * @since   2017-10-18
+ */
+public class EmailDetailServiceImplTest {
+
+    @Mock
+    EmailDetailRepository mockEmailDetailRepository;
+
+    @InjectMocks
+    EmailDetailService emailDetailServiceUnderTest = new EmailDetailServiceImpl();
+
+    /**
+     * Sets up objects and mocks external dependencies needed
+     * for the tests.
+     *
+     * (Long emailId, String emailAddress, EmailType emailType, Boolean isVerified) {
+     *
+     */
+    @Before
+    public void setUp() throws Exception {
+
+        MockitoAnnotations.initMocks(this);
+
+        EmailDetail emailDetail = new EmailDetail(Long.valueOf(1),
+                                                  "test@firstdigital.financial",
+                                                  EmailType.PRIMARY,
+                                                  true);
+
+        when(mockEmailDetailRepository.findDistinctByEmailIdEquals(Long.valueOf(1))).thenReturn(emailDetail);
+        when(mockEmailDetailRepository.findDistinctByEmailAddressEquals("test@firstdigital.financial")).thenReturn(emailDetail);
+        when(mockEmailDetailRepository.save(emailDetail)).thenReturn(emailDetail);
+
+    }
+
+    /**
+     * Tests EmailDetailServiceImpl.findDistinctByEmailIdEquals()
+     */
+    @Test
+    public void findDistinctByEmailIdEquals_EmailIsFound_Passes() throws Exception {
+        Assert.assertEquals("EmailDetailServiceImpl.findDistinctByEmailIdEquals() has failed",
+                "test@firstdigital.financial",
+                emailDetailServiceUnderTest.findDistinctByEmailIdEquals(Long.valueOf(1)).getEmailAddress());
+    }
+
+    /**
+     * Tests EmailDetailServiceImpl.findDistinctByEmailAddressEquals()
+     */
+    @Test
+    public void findDistinctByEmailAddressEquals_EmailIsFound_Passes() throws Exception {
+        Assert.assertEquals("EmailDetailServiceImpl.findDistinctByEmailAddressEquals() has failed",
+                "test@firstdigital.financial",
+                emailDetailServiceUnderTest.findDistinctByEmailAddressEquals("test@firstdigital.financial").getEmailAddress());
+    }
+
+    /**
+     * Tests EmailDetailServiceImpl.saveEmailDetail()
+     */
+    @Test
+    public void saveEmailDetail_EmailIsSaved_Passes() throws Exception {
+        EmailDetail emailDetail2 = new EmailDetail(Long.valueOf(2),
+                "test2@firstdigital.financial",
+                EmailType.PRIMARY,
+                true);
+
+        emailDetailServiceUnderTest.saveEmailDetail(emailDetail2);
+
+        //TODO: Find a way to test the above CrudRepository save...
+
+    }
+
+}
