85e5c78a31dd217597f00aec7782f34170f7971c
Andy McCall <mailme@andymccall.co.uk>
2017-10-18 15:09:41 +0100

    Added Application.main back using test.properties

--- src/test/java/financial/firstdigital/online/ApplicationTest.java
+++ src/test/java/financial/firstdigital/online/ApplicationTest.java
@@ -2,14 +2,29 @@ package financial.firstdigital.online;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.test.context.TestPropertySource;
 
 import javax.validation.constraints.AssertTrue;
 
 import static org.junit.Assert.*;
 
+/**
+ * The ApplicationTest class is the class that
+ * tests the Application class
+ *
+ * @author  Andy McCall
+ * @version 0.1
+ * @since   2017-10-18
+ */
+@TestPropertySource(
+        locations = "classpath:test.properties")
 public class ApplicationTest {
+
+    /**
+     * Test Application.main()
+     */
     @Test
     public void main() throws Exception {
-        Assert.assertTrue(true);
+        Application.main(new String[] {});
     }
 }
